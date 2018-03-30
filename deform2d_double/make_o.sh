e615340610a87240ffbd7f8063cdaeeaf78448d5
zhujiagang <1058776021@qq.com>
2018-03-30 09:07:44 +0800

    first commit gradient check

---
+++ deform2d_double/make_o.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd lib
+nvcc -c -o deform_conv2d_cuda_kernel.cu.o deform_conv2d_cuda_kernel.cu \
+     -I/home/lilin/anaconda3/lib/python3.6/site-packages/torch/lib/include/  \
+     -I/home/lilin/anaconda3/lib/python3.6/site-packages/torch/lib/include/TH  \
+     -x cu -Xcompiler -fPIC -std=c++11 -Wno-deprecated-gpu-targets
+cd ..
+CC=g++ /home/lilin/anaconda3/bin/python build.py
